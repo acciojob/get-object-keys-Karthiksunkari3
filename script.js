b0e7ee51ab32f9f93098552a1440352d34bcf982
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-14 00:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const  student = {
+	name : "Karthik Sunkari";
+};
+
+Object.protopyte.getKeys = function() {
+	return Object.keys(this);
+	
+}
+console.log(student.getKeys());
